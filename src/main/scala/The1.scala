1ff75b9e2e4cfbc9b101e46c5c5a83285d747ee6
730Line <3055524553@qq.com>
2024-10-11 12:53:23 +0800

    first commit

---
+++ src/main/scala/The1.scala
@@ -0,0 +1,13 @@
+object The1 {
+  def main(args: Array[String]): Unit = {
+    var n = 1+2.0
+    print(n)
+
+    var n2 :Double=1.0
+    var n4 :Byte=1
+    var n5 :Int=n4
+    var n6 :Byte=1
+    var c1 :Char=1
+  }
+
+}
